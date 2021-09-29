bff86ca4e2d3168219ef0e4d04a0521578189eae
GitaSeptiaPermata <gitaas.permata@gmail.com>
2021-09-29 22:06:47 +0700

    tugas webpro pustaka-booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class contoh1 extends CI_controller 
+{
+    public function index()
+    {
+    echo "<h1>perkenalkan</h1>";
+    echo "Nama saya gita septia permata saya tinggal di daerah cibinong olahraga yang saya sukai adalah berenang";
+    }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Gita</li>
+ <li>Nama Belakang : Septia</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Perumahan Cibinong City Blok K no 4</li>
+ </ul>
+
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Bogor</li>
+ </ul>
+
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Bulutangkis</li>
+ <li>Berenang</li>
+ </ul>
+</section>
